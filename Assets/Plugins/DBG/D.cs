bc9f6ba5cb4a680fac4a2f24597e73764f23ae15
Thorbjørn Nielsen <torybash@gmail.com>
2016-11-04 22:38:59 +0100

    Added DBG utility

---
+++ Assets/Plugins/DBG/D.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System;
+using UnityDBG;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class D {
+
+	public delegate void DebugLogger(object message, string tag = "");
+	public static DebugLogger LogDelegate;
+
+
+	[SerializeField] private string assetPath = "Assets/Plugins/DBG/DBGTags.asset";
+
+    public static bool _initialized = false;
+
+	private static D s_instance;	
+	private static D sInstance
+	{
+		get
+		{
+			if (s_instance == null) s_instance = new D();
+			return s_instance;
+		}
+	} 
+
+
+    public static void Init() {
+        Debug.Log("Initializing D!");
+
+		LogDelegate += DBG.Log;
+
+        if (UnityDBG.DBG.DbgTags == null) {
+            SetDBGTagReference();
+        }
+
+        _initialized = true;
+    }
+
+    private static void SetDBGTagReference() {
+
+		DBGTags dbgTags = AssetDatabase.LoadAssetAtPath<DBGTags>(sInstance.assetPath);
+		//DBGTags dbgTags = Resources.LoadAssetAtPath<DBGTags>(assetPath);;
+#if UNITY_EDITOR
+
+		if (dbgTags == null) {
+            dbgTags = ScriptableObject.CreateInstance<DBGTags>();
+            AssetDatabase.CreateAsset(dbgTags, sInstance.assetPath);
+        }
+#endif
+        UnityDBG.DBG.DbgTags = dbgTags;
+    }
+
+
+
+    public static DebugLogger Log {
+        get {
+            if (!_initialized) Init();
+            return LogDelegate;
+        }
+    }
+
+    //public static DebugLogger LogWarning {
+    //    get {
+    //        Check();
+    //        return LogWarningDelegate;
+    //    }
+    //}
+
+    //public static DebugLogger LogError {
+    //    get {
+    //        Check();
+    //        return LogErrorDelegate;
+    //    }
+    //}
+}
---
+++ UnityDBG/UnityDBG/DBG.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+
+using UnityEngine;
+
+
+
+namespace UnityDBG {
+    public class DBG {
+
+        private static DBGTags dbgTags;
+        public static DBGTags DbgTags {
+            get { return dbgTags; }
+            set { dbgTags = value; }
+        }
+
+        private static string nextMsgTag = "";
+
+
+        public static void Log(object obj, string tag = "") {
+           
+            DBGTag dbgTag = dbgTags.GetTag(tag);
+            string str  = (string) obj;
+            if (dbgTag != null) str = ApplyTag(str, dbgTag);
+            if (str != null) Debug.Log(str);
+        }
+
+        //public static void Log(object obj) {
+        //    obj = nextMsgTag + obj;
+        //    obj = CheckAndUseTag(obj);
+        //    if (obj != null) Debug.Log(obj);
+        //}
+        //public static void Log(object obj, UnityEngine.Object context) {
+        //    obj = CheckAndUseTag(obj);
+        //    if (obj != null) Debug.Log(obj, context);
+        //}
+
+        //public static void LogWarning(object obj) {
+        //    obj = CheckAndUseTag(obj);
+        //    if (obj != null) Debug.LogWarning(obj);
+        //}
+        //public static void LogError(object obj) {
+        //    obj = CheckAndUseTag(obj);
+        //    if (obj != null) Debug.LogError(obj);
+        //}
+
+        //public static void PrepareLog(string tag) {
+        //    nextMsgTag = tag;
+        //}
+
+
+        private static string ApplyTag(string msg, DBGTag dbgTag) {
+            if (!dbgTag.show) {
+                return null;
+            }
+
+            msg = "[" + dbgTag.tag + "]" + msg;
+
+            //Bold tag text
+            msg = msg.Insert(0, "<b>");
+            msg = msg.Insert(msg.IndexOf("]".ToCharArray()[0]) + 1, "</b>");
+
+            //Color text
+            msg = "<color=" + dbgTag.GetHexColor() + ">" + msg + "</color>";            
+            return msg;
+        }
+    }
+
+
+    [System.Serializable]
+    public class DBGTag {
+        public string tag;
+        public bool show;
+        public Color color;
+        [NonSerialized] private string colorHex = "";
+
+        public string GetHexColor() {
+            if (colorHex.Length == 0) {
+                Color32 color32 = (Color32)color;
+                colorHex = "#" + color32.r.ToString("X2") + color32.g.ToString("X2") + color32.b.ToString("X2");
+            }
+            return colorHex;
+        }
+    }
+
+    [System.Serializable]
+    public class DBGTags : ScriptableObject {
+        public List<DBGTag> tags;
+
+        public DBGTag GetTag(string tag) {
+            foreach (DBGTag dbgTag in tags) {
+                if (dbgTag.tag == tag) {
+                    return dbgTag;
+                }
+            }
+            return null;
+        }
+    }
+}
